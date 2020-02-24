fdf8f55d37e4872be1e04d7f230092a0d91bf11d
佐々奈津美 <sasanatsumi@sasanatsuminoMacBook-Pro.local>
2020-02-24 20:38:11 +0900

    初めてのコミット

---
+++ web/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
